ca8e8ba2e50ce97db33b314f5641e2efe0a59feb
CorteZzz137 <45366251+CorteZzz137@users.noreply.github.com>
2020-12-21 15:28:53 +0300

    Add files via upload

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,32 +2,40 @@
 #include "..//task4/task4.h"
 
 
-TEST(Task4, Test1) {
+TEST(Task4_1, Test1) {
 	string s = "      df        asdasdasd  df    sdsdsd  df    .";
 	delete_last(s);
 	EXPECT_TRUE(s == "asdasdasd sdsdsd");
 }
 
-TEST(Task4, Test2) {
+TEST(Task4_1, Test2) {
+	string s = "         .";
+	delete_last(s);
+	EXPECT_TRUE(s == "");
+}
+
+TEST(Task4_2, Test1) {
 	string s = "123 12345 123456 1234567";
 	replace_last_and_first(s);
 	EXPECT_TRUE(s == "321 52341 623451 7234561");
 }
 
-TEST(Task4, Test3) {
+TEST(Task4_2, Test2) {
 	string s = "";
 	replace_last_and_first(s);
 	EXPECT_TRUE(s == "");
 }
 
-TEST(Task4, Test4) {
-	string s = "         ";
+TEST(Task4_2, Test3) {
+	string s = "        ";
 	replace_last_and_first(s);
 	EXPECT_TRUE(s == "");
 }
 
-TEST(Task4, Test5) {
+TEST(Task4_2, Test4) {
 	string s = " hhh      ";
 	replace_last_and_first(s);
 	EXPECT_TRUE(s == "hhh");
 }
+
+
--- task4/task4.h
+++ task4/task4.h
@@ -15,19 +15,21 @@ void delete_last(string& s) {
 	while (ss >> tmp) {
 		words.push_back(tmp);
 	}
-	string delWord = words.back();
 	s = "";
-	for (vector<string>::iterator it = words.begin(); it != words.end(); ++it) {
+	if (words.size() > 0) {
+		string delWord = words.back();
+		for (vector<string>::iterator it = words.begin(); it != words.end(); ++it) {
 
-		if (*it == delWord) {
-			it = words.erase(it);
+			if (*it == delWord) {
+				it = words.erase(it);
+			}
+			if (it != words.end())
+				s += *it + " ";
+			else
+				break;
 		}
-		if (it != words.end())
-			s += *it + " ";
-		else
-			break;
+		s = s.substr(0, s.size() - 1);
 	}
-	s = s.substr(0, s.size() - 1);
 }
 
 void replace_last_and_first(string& s) {
@@ -38,9 +40,11 @@ void replace_last_and_first(string& s) {
 		words.push_back(tmp);
 	}
 	s = "";
-	for (vector<string>::iterator it = words.begin(); it != words.end(); ++it) {
-		*it = it->back() + it->substr(1, it->length() - 2) + (*it)[0];
-		s += *it + " ";
+	if (words.size() > 0) {
+		for (vector<string>::iterator it = words.begin(); it != words.end(); ++it) {
+			*it = it->back() + it->substr(1, it->length() - 2) + (*it)[0];
+			s += *it + " ";
+		}
+		s = s.substr(0, s.size() - 1);
 	}
-	s = s.substr(0, s.size() - 1);
 }
